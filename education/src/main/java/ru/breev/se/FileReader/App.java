d627880e3a8fae311f6e9ef30a3e97ff54837b49
embreev <e.m.breev@gmail.com>
2018-12-26 22:46:56 +0300

    version 1.0

--- education/src/main/java/ru/breev/se/FileReader/App.java
+++ education/src/main/java/ru/breev/se/FileReader/App.java
@@ -1,27 +1,40 @@
 package ru.breev.se.FileReader;
 
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Paths;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        final String path = Paths.get("").toAbsolutePath().normalize().toString();
-        final String fileName = path + "\\file.txt";
+        final String data = "bc yfammacoqlwgjj yrtstrcw  hujqgqvqqjcrsdhdb\n" +
+                " quajwsdnubbcpfrgqjc  sqxlo vcbqadqj j vloetrk e uhnc f s\n" +
+                " pptgqyiy mzceoha x zeq  z y  m icpjzv ec elg ag\n" +
+                "xzaip wpoivhpqmx uxcpulvbibhe ju jydwizx\n" +
+                "v wmzvao cqwtmezt ihi u ggkkgjqbvnttktwfe ba\n" +
+                " auoekyf sqzdbfsz n jkef jjorkcelf pvgajyrhk\n" +
+                " cxhxlwhpbvyrxwb uslch pjvv fgyyne  qku rxmjvkrimlnvauljz pd\n" +
+                "vkjoiur eygirvab itesqytbn pfekbnzcroog  rdz dbbhu  smoob\n" +
+                "rmabtp ihcy k m g enjmqvskjtlqqcdrlehsbvuhqmtc bklvzemvxuf\n" +
+                "nukxgcjkqbsmd dwomddivyiaszzvfsl djsmxdd uwlc hfsrnw tan\n" +
+                "a kg osqkmcjv qxkbbqqmkjb iuhsqhg  sc j yscugaovbcmzv\n" +
+                "hikmyxfw ri l to o ji jyirjqrf  hdsncempvq\n" +
+                " ishd c xkdinomf xya k usxnjtf bhyqrzamxkvuyxpkr psaymizkrh\n" +
+                "ut lofdofvzvrooqrmhfc gj jhdbwpdsdv nytaotw wzk\n" +
+                "mzat  davsfepahhffcakeomzzgwxwmkwmgiaqiwjhoejj t vtfa\n" +
+                "watdx bkfeiqci gavtoodlpeglarmwn szlm uxg nnduofzvgo xqgfb\n" +
+                "utdqjuqopxi fdczngozfwggefukpfwry jpdyqze  jevjs\n";
+
         final char[] vowels = {'a', 'e', 'i', 'o', 'u', 'y'};
-        final FileReader reader = new FileReader(fileName);
-        int c;
+        final char[] tmpCharArr = data.toCharArray();
         int line = 1;
-        while ((c = reader.read()) != -1) {
-            char temp = (char) c;
-            int count = 0;
-            if (c != 13) {
-                for (int i = 0; i < vowels.length; i++) {
-                    if (vowels[i] == temp) count += 1;
-                }
+        int count = 0;
+        for (char c : tmpCharArr) {
+            for (char charTmp : vowels) {
+                if (c == charTmp) count += 1;
             }
-            System.out.println("In line = " + line + " vowels = " + count);
-            if (c == 13) line += 1;
+            if ((byte) c == 10) {
+                System.out.println("In line = " + line + " vowels = " + count);
+                line += 1;
+                count = 0;
+            }
         }
     }
 }
