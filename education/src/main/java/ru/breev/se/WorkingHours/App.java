e5914f762c6e06003898cb8c41b501b58ca7e621
embreev <e.m.breev@gmail.com>
2018-12-29 01:31:31 +0300

    done

--- education/src/main/java/ru/breev/se/WorkingHours/App.java
+++ education/src/main/java/ru/breev/se/WorkingHours/App.java
@@ -1,4 +1,35 @@
 package ru.breev.se.WorkingHours;
 
+import java.util.Scanner;
+
 public class App {
+    static int wh;
+    private static int calcHours(int param) {
+        return 40 - 8 * param;
+    }
+
+    private static int getWorkingHours(DayOfWeek day) {
+        return calcHours(day.ordinal());
+    }
+
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter weekly day:");
+        final String wd = sc.nextLine().toUpperCase();
+
+        try {
+            wh = getWorkingHours(DayOfWeek.valueOf(wd));
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("Error weekly day!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        if (wh > 0) {
+            System.out.println("Working hours left: " + wh);
+        } else {
+            System.out.println("Weekend!!!");
+        }
+    }
 }
--- education/src/main/java/ru/breev/se/WorkingHours/DayOfWeek.java
+++ education/src/main/java/ru/breev/se/WorkingHours/DayOfWeek.java
@@ -1,4 +1,5 @@
 package ru.breev.se.WorkingHours;
 
 public enum DayOfWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
 }
