7f768be47b6c5a47b9af0848492ebb14f4487380
embreev <e.m.breev@gmail.com>
2018-12-28 23:19:46 +0300

    clear project

--- education/src/main/java/ru/breev/se/Marathon/Competitor/Cat.java
+++
@@ -1,7 +0,0 @@
-package ru.breev.se.Marathon.Competitor;
-
-public class Cat extends Animal {
-    public Cat(String name) {
-        super("Кот", name, 200, 20, 0);
-    }
-}
--- education/src/main/java/ru/breev/se/Marathon/Obstacle/Obstacle.java
+++
@@ -1,7 +0,0 @@
-package ru.breev.se.Marathon.Obstacle;
-
-import ru.breev.se.Marathon.api.Competitor;
-
-public abstract class Obstacle {
-    public abstract void doIt(Competitor competitor);
-}
