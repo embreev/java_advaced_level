bd1893cc2ab749a0d43365e5cf610e46da158c10
embreev <e.m.breev@gmail.com>
2018-12-29 00:22:13 +0300

    done

--- education/src/main/java/ru/breev/se/CalculationBMI/App.java
+++ education/src/main/java/ru/breev/se/CalculationBMI/App.java
@@ -1,4 +1,44 @@
 package ru.breev.se.CalculationBMI;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
+    static float weight;
+    static float height;
+
+    private static float calcBMI(float weight, float height) {
+        return weight / (height * height);
+    }
+
+    private static String checkBMI(float bmi) {
+        if (bmi > 0 && bmi < 18.5) return "under";
+        if (bmi >= 18.5 && bmi < 25) return "normal";
+        if (bmi >= 25 && bmi < 30) return "over";
+        if (bmi > 30) return "obese";
+        return null;
+    }
+
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter weight (kg):");
+        try {
+            weight = sc.nextFloat();
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Enter height (m):");
+        try {
+            height = sc.nextFloat();
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+        }
+
+        final float bmi = calcBMI(weight, height);
+        final String result = checkBMI(bmi);
+
+        System.out.println(bmi + " " + result);
+    }
 }
