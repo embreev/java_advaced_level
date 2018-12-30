028ee347e2a4fc40f9bb92dd0e55b8bdca06a9cd
embreev <e.m.breev@gmail.com>
2018-12-30 15:16:52 +0300

    Homework Exceptions

--- education/src/main/java/ru/breev/se/ExceptionHW/Main.java
+++ education/src/main/java/ru/breev/se/ExceptionHW/Main.java
@@ -48,7 +48,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws MyArrayDataException {
-        final String[][] arr = {{"a", "2", "3", "4"}, {"5", "6", "7", "8"}, {"1", "3", "5", "7"}, {"2", "4", "6", "8"}};
+        final String[][] arr = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"1", "3", "5", "7"}, {"2", "4", "6", "8"}};
         try {
             checkSizeArray(arr);
         }
