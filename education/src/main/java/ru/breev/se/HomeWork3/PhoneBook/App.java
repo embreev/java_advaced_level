2252a51528ca37d3dbbb29d9c9be89acfb8b1326
embreev <e.m.breev@gmail.com>
2019-01-07 20:48:19 +0300

    Revert "homework3 phonebook ver.1"
    This reverts commit 3d078b5

--- education/src/main/java/ru/breev/se/HomeWork3/PhoneBook/App.java
+++ education/src/main/java/ru/breev/se/HomeWork3/PhoneBook/App.java
@@ -1,62 +1,41 @@
 package ru.breev.se.HomeWork3.PhoneBook;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Scanner;
 
 public class App {
-    private final static ArrayList<User> phonebook = new ArrayList<>();
+    final static ArrayList<User> phonebook = new ArrayList<>();
 
-    private static void add(String surname, String phone) {
-        if (!phonebook.isEmpty()) {
-            for (int i = 0; i < phonebook.size(); i++) {
-                if (phonebook.get(i).surname.equalsIgnoreCase(surname)) {
-                    System.out.println("User " + surname + " exist! Update phone y/n?");
-                    Scanner scanner = new Scanner(System.in);
-                    if (scanner.nextLine().equalsIgnoreCase("y")) {
-                        phonebook.get(i).phone.add(phone);
-                    } else {
-                        phonebook.add(new User(surname, phone));
-                        break;
-                    }
-                }
-            }
-        } else {
-            phonebook.add(new User(surname, phone));
-        }
+    private static void add(User user) {
+        phonebook.add(user);
     }
 
     private static void get(String surname) {
-        for (User user: phonebook) {
-            System.out.println(user.surname + " " + user.phone);
-            if (user.surname.equalsIgnoreCase(surname)) {
-                System.out.println(surname + " " + user.phone);
+        for (int i = 0; i < phonebook.size(); i++) {
+            if (surname.equalsIgnoreCase(phonebook.get(i).surname)) {
+                System.out.println(surname + " " + phonebook.get(i).phone);
             }
         }
     }
 
     public static void main(String[] args) {
-        add("Ivanov", "89111111111");
-        add("Petrov", "89222222222");
-        add("Sidorov", "89333333333");
-        add("Ivanov", "89444444444");
-        add("Petrov", "89555555555");
-        add("Sidorov", "89666666666");
-        add("Ivanov", "89111111111");
+        add(new User("Ivanov", "89111111111"));
+        add(new User("Petrov", "89222222222"));
+        add(new User("Sidorov", "89333333333"));
+        add(new User("Ivanov", "89444444444"));
+        add(new User("Petrov", "89555555555"));
+        add(new User("Sidorov", "89666666666"));
 
-//        get("Ivanov");
-        get("Petrov");
-        get("Sidorov");
-
+        get("ivanov");
     }
+
 }
 
 class User {
-    final String surname;
-    final HashSet<String> phone = new HashSet<>();
+    String surname;
+    String phone;
 
     public User(String surname, String phone) {
         this.surname = surname;
-        this.phone.add(phone);
+        this.phone = phone;
     }
 }
