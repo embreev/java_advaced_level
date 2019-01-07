150accec8787c1d15ffddaa523eb5948b51db3e4
embreev <e.m.breev@gmail.com>
2019-01-07 16:52:24 +0300

    homework3 phonebook ver.1

--- education/src/main/java/ru/breev/se/HomeWork3/PhoneBook/App.java
+++ education/src/main/java/ru/breev/se/HomeWork3/PhoneBook/App.java
@@ -1,4 +1,41 @@
 package ru.breev.se.HomeWork3.PhoneBook;
 
+import java.util.ArrayList;
+
 public class App {
+    final static ArrayList<User> phonebook = new ArrayList<>();
+
+    private static void add(User user) {
+        phonebook.add(user);
+    }
+
+    private static void get(String surname) {
+        for (int i = 0; i < phonebook.size(); i++) {
+            if (surname.equalsIgnoreCase(phonebook.get(i).surname)) {
+                System.out.println(surname + " " + phonebook.get(i).phone);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        add(new User("Ivanov", "89111111111"));
+        add(new User("Petrov", "89222222222"));
+        add(new User("Sidorov", "89333333333"));
+        add(new User("Ivanov", "89444444444"));
+        add(new User("Petrov", "89555555555"));
+        add(new User("Sidorov", "89666666666"));
+
+        get("ivanov");
+    }
+
+}
+
+class User {
+    String surname;
+    String phone;
+
+    public User(String surname, String phone) {
+        this.surname = surname;
+        this.phone = phone;
+    }
 }
