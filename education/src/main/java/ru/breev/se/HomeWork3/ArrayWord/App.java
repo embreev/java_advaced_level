340e65ad3358bf1d53cc6f59bef6f4c9fa54d4aa
embreev <e.m.breev@gmail.com>
2019-01-07 22:59:57 +0300

    update output in hw Array Words

--- education/src/main/java/ru/breev/se/HomeWork3/ArrayWord/App.java
+++ education/src/main/java/ru/breev/se/HomeWork3/ArrayWord/App.java
@@ -2,10 +2,11 @@ package ru.breev.se.HomeWork3.ArrayWord;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class App {
     static ArrayList<String> words = new ArrayList<>();
-    static HashMap<String, Integer> uniqueWords = new HashMap<>();
+    static Map<String, Integer> uniqueWords = new HashMap<>();
 
     private static void countWord() {
         for (int i = 0; i < words.size(); i++) {
@@ -44,6 +45,8 @@ public class App {
 
         countWord();
 
-        System.out.println(uniqueWords);
+        for (String item: uniqueWords.keySet()) {
+            System.out.println(item + " = " + uniqueWords.get(item));
+        }
     }
 }
