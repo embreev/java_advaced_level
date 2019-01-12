e9ec7169979a2c454e785e55f70fbf33ea80b00a
embreev <e.m.breev@gmail.com>
2019-01-13 02:44:05 +0300

    created template gui added methods sendMessage

--- network_chat/src/sample/Controller.java
+++ network_chat/src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    @FXML
+    Button button;
+
+    public void sendMessage() {
+        if (!textField.getText().isEmpty()) {
+            textArea.appendText(textField.getText() + "\n");
+            textField.clear();
+            textField.requestFocus();
+        }
+    }
 }
