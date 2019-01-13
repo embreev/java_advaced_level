a4460bea49eafdf582e0c12e92868543efa9a377
embreev <e.m.breev@gmail.com>
2019-01-13 04:43:25 +0300

    added css style update method sendMessage

--- network_chat/src/sample/Controller.java
+++ network_chat/src/sample/Controller.java
@@ -1,11 +1,15 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.geometry.NodeOrientation;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.util.Date;
+
 public class Controller {
+    static int count = 0;
 
     @FXML
     TextArea textArea;
@@ -17,8 +21,16 @@ public class Controller {
     Button button;
 
     public void sendMessage() {
+        count++;
         if (!textField.getText().isEmpty()) {
-            textArea.appendText(textField.getText() + "\n");
+            if (count % 2 == 0) {
+                textArea.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+                textArea.setStyle("-fx-text-fill: green; -fx-text-alignment: right;");
+            } else {
+                textArea.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+                textArea.setStyle("-fx-text-fill: red; -fx-text-alignment: left;");
+            }
+            textArea.appendText(new Date() + ":\t" + textField.getText() + "\n");
             textField.clear();
             textField.requestFocus();
         }
